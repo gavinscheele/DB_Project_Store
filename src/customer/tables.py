ea164f57931172a2302c8d762c82eefeaa6c8b47
gdscheele <gdscheele@gmail.com>
2015-12-02 00:41:30 -0500

    Placing an order is nearly done.

--- src/customer/tables.py
+++ src/customer/tables.py
@@ -18,3 +18,20 @@ class ProductTable(tables.Table):
 		attrs = {"class": "paleblue"}
 		fields = ("name", "description", "price", "stockQuantity", "discount", "selection")
 		sequence = ("name", "description", "price", "stockQuantity", "discount", "selection")
+
+
+# shows products that are in an order. Allows quantities to be edited.
+class ContainTable(tables.Table):
+	product_name = tables.Column(accessor="product.name")
+	product_description = tables.Column(accessor="product.description")
+	product_price = tables.Column(accessor="product.price")
+	product_stockQuantity = tables.Column(accessor="product.stockQuantity")
+	order_quantity = tables.TemplateColumn('<input type="number" pattern="[0-9]*">', verbose_name="Quantity")
+
+
+	class Meta:
+		model = models.Contain
+		
+		attrs = {"class": "paleblue"}
+		fields = ("product_name", "product_description", "product_stockQuantity", "quantity")
+		sequence = ("product_name", "product_description", "product_stockQuantity", "quantity")
--- src/customer/views.py
+++ src/customer/views.py
@@ -6,9 +6,15 @@ from welcome import forms
 import json
 # Create your views here.
 def customer(request):
+	#get logged in user
 	UserID = request.session.get('UserID')
+	current_user = models.User.objects.get(id=UserID)
+
+	#set variable for data passed by form
 	product_name = request.POST.get('Search_Products')
 	selected_products = request.POST.get('selected_products')
+
+	# change table to show products that match search query, or all if blank
 	if product_name:
 		table = tables.ProductTable(models.Product.objects.filter(name__contains=product_name))
 	else:
@@ -16,14 +22,32 @@ def customer(request):
 
 	if selected_products:
 		p = json.loads(selected_products)
-		print(p[0])
+		# create order, add products to order
+		new_order = models.customerOrder(user=current_user, paid=False)
+		new_order.save()
+		for i in p:
+			contain = models.Contain(customerOrder=new_order, product=models.Product.objects.get(id=i), quantity=0)
+			contain.save()
 
-	print(request)
+		#switch out table to show products in the order.
+		#allow quantities of products to be edited.
+		table = tables.ContainTable(models.Contain.objects.all())
+
+	#FOR TESTING ONLY ######################################
+	print(models.Contain.objects.all())
+	table = tables.ContainTable(models.Contain.objects.all())
+	########################################################
+
+	show_search = False
+	tally_products = True
 	RequestConfig(request).configure(table)
 	context = {
-		'table'	:	table,
-		'product_search_form' : forms.ProductSearchForm,
-		'select_products_form' : forms.ProductSelectForm
+		'table'							: table,
+		'product_search_form' 			: forms.ProductSearchForm,
+		'select_products_form' 			: forms.ProductSelectForm,
+		'user_name'						: current_user.name,
+		'show_search'					: show_search,
+		'tally_products'				: tally_products
 
 	}
 	return render(request, "customer.html", context)
