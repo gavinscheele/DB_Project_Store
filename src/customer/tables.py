760f84e71be891dccaae2ae4e7f8983f0998bf81
gdscheele <gdscheele@gmail.com>
2015-12-02 20:39:25 -0500

    Customer ordering is done.

--- src/customer/tables.py
+++ src/customer/tables.py
@@ -16,22 +16,24 @@ class ProductTable(tables.Table):
 	class Meta:
 		model = models.Product
 		attrs = {"class": "paleblue"}
-		fields = ("name", "description", "price", "stockQuantity", "discount", "selection")
-		sequence = ("name", "description", "price", "stockQuantity", "discount", "selection")
+		fields = ("name", "supplier", "description", "price", "stockQuantity", "selection")
+		sequence = ("name", "supplier", "description", "price", "stockQuantity", "selection")
 
 
 # shows products that are in an order. Allows quantities to be edited.
 class ContainTable(tables.Table):
+	order_id = tables.Column(accessor="customerOrder.pk")
+	product_id = tables.Column(accessor="product.pk")
 	product_name = tables.Column(accessor="product.name")
 	product_description = tables.Column(accessor="product.description")
 	product_price = tables.Column(accessor="product.price")
 	product_stockQuantity = tables.Column(accessor="product.stockQuantity")
-	order_quantity = tables.TemplateColumn('<input type="number" pattern="[0-9]*">', verbose_name="Quantity")
-
+	order_quantity = tables.TemplateColumn('<input type="number" pattern="[0-9]*" value="0">', verbose_name="Quantity")
+	product_supplier = tables.Column(accessor="product.supplier.pk")
 
 	class Meta:
 		model = models.Contain
 		
 		attrs = {"class": "paleblue"}
-		fields = ("product_name", "product_description", "product_stockQuantity", "quantity")
-		sequence = ("product_name", "product_description", "product_stockQuantity", "quantity")
+		fields = ("order_id", "product_id","product_supplier", "product_name", "product_description", "product_stockQuantity")
+		sequence = ("order_id", "product_id", "product_supplier", "product_name", "product_description", "product_stockQuantity")
--- src/customer/views.py
+++ src/customer/views.py
@@ -4,8 +4,12 @@ from welcome import models
 from . import tables
 from welcome import forms
 import json
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 # Create your views here.
 def customer(request):
+
 	#get logged in user
 	UserID = request.session.get('UserID')
 	current_user = models.User.objects.get(id=UserID)
@@ -13,33 +17,80 @@ def customer(request):
 	#set variable for data passed by form
 	product_name = request.POST.get('Search_Products')
 	selected_products = request.POST.get('selected_products')
+	product_updates = request.POST.get('update_info')
+
+	#variables to pass to the DOM
+	show_proceed_to_payment_button = False
+	tally_products = False
+
 
 	# change table to show products that match search query, or all if blank
 	if product_name:
-		table = tables.ProductTable(models.Product.objects.filter(name__contains=product_name))
+		table = tables.ProductTable(models.Product.objects.filter(name__contains=product_name, active=True))
 	else:
-		table = tables.ProductTable(models.Product.objects.all())
+		table = tables.ProductTable(models.Product.objects.filter(active=True))
+		show_proceed_to_payment_button = True
 
 	if selected_products:
+		show_proceed_to_payment_button = False
+		tally_products = True
 		p = json.loads(selected_products)
 		# create order, add products to order
 		new_order = models.customerOrder(user=current_user, paid=False)
 		new_order.save()
 		for i in p:
-			contain = models.Contain(customerOrder=new_order, product=models.Product.objects.get(id=i), quantity=0)
+			contain = models.Contain(customerOrder=new_order, product=models.Product.objects.get(id=i, active=True), quantity=0)
 			contain.save()
 
 		#switch out table to show products in the order.
 		#allow quantities of products to be edited.
-		table = tables.ContainTable(models.Contain.objects.all())
+		table = tables.ContainTable(models.Contain.objects.filter(customerOrder=new_order))
 
-	#FOR TESTING ONLY ######################################
-	print(models.Contain.objects.all())
-	table = tables.ContainTable(models.Contain.objects.all())
-	########################################################
 
+	if product_updates:
+		show_proceed_to_payment_button = False
+		product_updates = json.loads(product_updates)
+		index = 0
+		saved = False
+		while index < product_updates["length"]:
+			ord_id = product_updates[str(index)][0]
+			pr_id = product_updates[str(index)][1]
+			quant = product_updates[str(index)][2]
+			try:
+				contain_obj = models.Contain.objects.get(product=models.Product.objects.get(id=int(pr_id), active=True), customerOrder=models.customerOrder.objects.get(pk=int(ord_id)))
+			except models.Product.DoesNotExist or models.Contain.DoesNotExist or models.customerOrder.DoesNotExist:
+				contain_obj = None
+			if contain_obj:
+				contain_obj.quantity = int(quant)
+				order_obj = models.customerOrder.objects.get(pk=int(ord_id))
+				order_obj.paid = True
+				prod_obj = models.Product.objects.get(pk=int(pr_id))
+				prod_obj.stockQuantity = prod_obj.stockQuantity - int(quant)
+				if prod_obj.stockQuantity < 0:
+					order_obj.delete()
+					messages.add_message(request, messages.INFO, 'You ordered more than the stock quantity. Returning to customer page.')
+					return HttpResponseRedirect(reverse('customer'))
+
+				if int(quant) == 0:
+					order_obj.delete()
+					messages.add_message(request, messages.INFO, 'You can\'t order 0 products. Returning to customer page.')
+					return HttpResponseRedirect(reverse('customer'))
+
+				order_obj.save()
+				contain_obj.save()
+				prod_obj.save()
+				saved = True
+
+			else:
+				print("Product or Contain or CustomerOrder does not exist!")
+			index += 1;
+
+		if saved:
+			messages.add_message(request, messages.INFO, 'Order Saved.')
+		return HttpResponseRedirect(reverse('customer'))
+			
+	# if update_order_quantity:
 	show_search = False
-	tally_products = True
 	RequestConfig(request).configure(table)
 	context = {
 		'table'							: table,
@@ -47,7 +98,9 @@ def customer(request):
 		'select_products_form' 			: forms.ProductSelectForm,
 		'user_name'						: current_user.name,
 		'show_search'					: show_search,
-		'tally_products'				: tally_products
+		'tally_products'				: tally_products,
+		'update_order_form'				: forms.UpdateOrderForm,
+		'selected_products'				: show_proceed_to_payment_button
 
 	}
 	return render(request, "customer.html", context)
