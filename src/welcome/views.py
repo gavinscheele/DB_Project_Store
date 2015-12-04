6d44125e73e589a41c7ec49b47a690cf799e7aa5
gdscheele <gdscheele@gmail.com>
2015-12-04 16:06:12 -0500

    fixed ordering 0 or less products. Fixed sorting of product table on the welcome page. added a show all button on welcome product table.

--- src/welcome/views.py
+++ src/welcome/views.py
@@ -7,10 +7,19 @@ from . import forms
 def welcome(request):
 	request.session['UserID'] = "guest" #Initially, ID is guest.
 	product_name = request.POST.get('Search_Products')
+	show_all = request.POST.get('show_all')
+
 	if product_name:
-		table = tables.ProductTable(models.Product.objects.filter(name__contains=product_name, active=True))
+		request.session['product_search_name'] = product_name
+
+	sorting = request.GET.get('sort')
+	print(sorting)
+
+	if (product_name or sorting) and not show_all:
+		table = tables.ProductTable(models.Product.objects.filter(name__contains=request.session.get('product_search_name'), active=True))
 	else:
 		table = tables.ProductTable(models.Product.objects.filter(active=True))
+
 
 	print(request)
 	RequestConfig(request).configure(table)
